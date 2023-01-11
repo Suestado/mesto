3925fb2f618353d61b2e385bd0155b9a48e7058d
Sergey Kiselev <sk.mails@yandex.ru>
2023-01-11 15:12:09 +0300

    bugs: ошибки валидации исправлены

--- scripts/index.js
+++ scripts/index.js
@@ -2,44 +2,29 @@ const
   popupContainer = document.querySelector('.popup__container'),
   editButton = document.querySelector('.profile__edit-button'),
   closeButton = document.querySelector('.popup__close'),
-  submitButton = document.querySelector('.popup__submit'),
-  popup = document.querySelector('.popup');
+  popup = document.querySelector('.popup'),
+  userName = document.querySelector('.profile__name'),
+  userDescription = document.querySelector('.profile__description'),
+  popupName = document.querySelector('.popup__input_field_name'),
+  popupDescription = document.querySelector('.popup__input_field_description');
+
 
 editButton.addEventListener('click', function () {
-  let
-    name = document.querySelector('.profile__name').textContent,
-    description = document.querySelector('.profile__description').textContent,
-    popupName = document.querySelector('.popup__input_name'),
-    popupDescription = document.querySelector('.popup__input_description');
-
-  popupName.value = name;
-  popupDescription.value = description;
+  popupName.value = userName.textContent;
+  popupDescription.value = userDescription.textContent;
   popup.style.display = 'flex';
   popup.classList.add('popup__animation');
 })
 
 closeButton.addEventListener('click', function () {
-  popupContainer.reset();
   popup.style.display = 'none';
 })
 
-popupContainer.onsubmit = function handleFormSubmit(evt) {
+popupContainer.addEventListener('submit', function handleFormSubmit(evt) {
   evt.preventDefault();
-  let
-    newName = document.querySelector('.popup__input_name').value,
-    newJob = document.querySelector('.popup__input_description').value;
-
-  document.querySelector('.profile__name').textContent = newName;
-  document.querySelector('.profile__description').textContent = newJob;
+  userName.textContent = popupName.value;
+  userDescription.textContent = popupDescription.value;
   popup.style.display = 'none';
-}
-
-window.addEventListener('click', function(e) {
-  if(e.target === popup) {
-    popupContainer.reset();
-    popup.style.display = 'none';
-  }
 })
-
 
 
