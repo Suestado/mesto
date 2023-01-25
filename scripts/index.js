5def3729a28658cfa2af0556aca4eb038c59c898
Sergey Kiselev <sk.mails@yandex.ru>
2023-01-25 13:24:25 +0300

    feat: Реализован функционал добавления пользовательских фото

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-// Стартовый сет картинок для страницы
+/*
+Стартовый сет картинок для страницы
+*/
 const initialCards = [
   {
     name: 'Архыз',
@@ -27,35 +29,88 @@ const initialCards = [
 ];
 
 
-// Реализация универсальной функции добавления карточки с местом на страницу
+/*
+Реализация универсальной функции добавления карточки с местом на страницу
+*/
 const
   photoSection = document.querySelector('.elements'),
   photoCardTemplate = document.querySelector('#photo-card').content;
 
-function newPhotoCardAdd (args) {
-  args.forEach(function(item) {
-    //наполняем элемент контентом
+function newPhotoCardAdd(args) {
+  args.forEach(function (item) {
+      //наполняем элемент контентом
     const photoCardItem = photoCardTemplate.querySelector('.element').cloneNode(true);
     photoCardItem.querySelector('.element__name').textContent = item.name;
     photoCardItem.querySelector('.element__image').src = item.link;
     photoCardItem.querySelector('.element__image').alt = item.name;
 
-    //добавляем элемент на страницу
+      //добавляем элемент на страницу
     photoSection.prepend(photoCardItem);
   })
 }
+
 newPhotoCardAdd(initialCards);
 
 
-//Реализация лайка на фото
+/*
+Реализация лайка на фото
+*/
 photoSection.addEventListener('click', function (evt) {
-  if(evt.target.classList.contains('element__like')) {
+  if (evt.target.classList.contains('element__like')) {
     toggleClass(evt.target, 'element__like_active')
   }
 });
 
 
-// Реализация редактирования данных профиля
+/*
+Реализация добавления пользовательских фото
+*/
+const
+  photoAddButton = document.querySelector('.profile__add-button'),
+  popupPhotoAdd = document.querySelector('.popup-photoAdd'),
+  photoInputPlaceDescription = document.querySelector('.popup-photoAdd__input_field_place'),
+  photoInputLink = document.querySelector('.popup-photoAdd__input_field_link'),
+  photoAddContainer = document.querySelector('.popup-photoAdd__card'),
+  photoAddCloseButton = document.querySelector('.popup-photoAdd__close');
+
+  //Открытие попапа
+photoAddButton.addEventListener('click', () => {
+  toggleClass(popupPhotoAdd, 'popup_visible');
+})
+
+  //Добавление фото
+photoAddContainer.addEventListener('submit', function addPhotoFunc(evt) {
+  evt.preventDefault();
+  if(!photoInputPlaceDescription.value || !photoInputLink.value) {
+    alert('Введите название и ссылку на фото')
+    return;
+  } else {
+    const userOwnPhoto = [{}];
+    userOwnPhoto[0].name = photoInputPlaceDescription.value;
+    userOwnPhoto[0].link = photoInputLink.value;
+
+    newPhotoCardAdd(userOwnPhoto);
+  }
+    //очистка формы
+  photoInputPlaceDescription.value = '';
+  photoInputLink.value = '';
+    //закрытие попапа
+  toggleClass(popupPhotoAdd, 'popup_visible');
+})
+
+  //закрытие попапа
+photoAddCloseButton.addEventListener('click', () => {
+  toggleClass(popupPhotoAdd, 'popup_visible');
+
+    //очистка формы
+  photoInputPlaceDescription.value = '';
+  photoInputLink.value = '';
+})
+
+
+/*
+Реализация редактирования данных профиля
+*/
 const
   popupContainer = document.querySelector('.popup__card'),
   editButton = document.querySelector('.profile__edit-button'),
@@ -66,21 +121,24 @@ const
   popupName = document.querySelector('.popup__input_field_name'),
   popupDescription = document.querySelector('.popup__input_field_description');
 
-function toggleClass (container, className) {
+function toggleClass(container, className) {
   container.classList.toggle(className);
-  }
+}
 
+//Открытие попапа и предзаполнение формы данными со страницы
 editButton.addEventListener('click', function () {
   popupName.value = userName.textContent;
   popupDescription.value = userDescription.textContent;
   toggleClass(popup, 'popup_visible');
 })
 
+//закрытие попапа
 closeButton.addEventListener('click', function handleFormSubmit(evt) {
   evt.preventDefault();
   toggleClass(popup, 'popup_visible');
 })
 
+//сохранение данных из формы на странице
 popupContainer.addEventListener('submit', function handleFormSubmit(evt) {
   evt.preventDefault();
   userName.textContent = popupName.value;
