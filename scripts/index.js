190e390d8e444959baf8f851a469004d5ecef24c
Sergey Kiselev <sk.mails@yandex.ru>
2023-02-15 16:10:21 +0300

    fix: Рефакторинг кода после ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 //переменные для добавления фото карточек
 const
-  photoAddForm = document.querySelector('#photoAdd-form'),
+  photoAddForm = document.forms['photoAdd-form'],
+  photoAddInputList = Array.from(photoAddForm.querySelectorAll('.popup__input')),
+  photoAddSubmitButton = photoAddForm.querySelector('.popup__submit'),
   photoSection = document.querySelector('.elements'),
   photoCardTemplate = document.querySelector('#photo-card').content,
   photoAddButton = document.querySelector('.profile__add-button'),
@@ -9,20 +11,26 @@ const
   photoInputLink = document.querySelector('.popup__input_type_photoAdd-link'),
   photoAddCloseButton = document.querySelector('.popup__close_type_photoAdd');
 
+
 //Переменные для просмотра фото
 const
   popupFullScreen = document.querySelector('.popup_type_photoFullScreen'),
   popupFullScreenPic = popupFullScreen.querySelector('.popup__image_type_photoFullScreen'),
   popupFullScreenFigcaption = popupFullScreen.querySelector('.popup__substring_type_photoFullScreen');
 
+
 //Переменные для редактирования данных профиля
 const
+  profileEditForm = document.forms['profileEdit-form'],
+  profileEditInputList = Array.from(profileEditForm.querySelectorAll('.popup__input')),
+  profileEditSubmitButton = profileEditForm.querySelector('.popup__submit'),
   editButton = document.querySelector('.profile__edit-button'),
-  popupForEditForm = document.querySelector('.popup_type_editForm'),
+  popupForProfileEditForm = document.querySelector('.popup_type_editForm'),
   userName = document.querySelector('.profile__name'),
   userDescription = document.querySelector('.profile__description'),
   popupForEditFormName = document.querySelector('.popup__input_type_editForm-name'),
   popupForEditFormDescription = document.querySelector('.popup__input_type_editForm-description');
+
 
 
 //Общий метод замены класса
@@ -30,15 +38,24 @@ function toggleClass(container, className) {
   container.classList.toggle(className);
 }
 
+
 //Метод открытия попапа через добавление класса
 function openPopup(container) {
   container.classList.add('popup_opened');
+
+  //установка обработчика на закрытие попапа через Escape
+  document.addEventListener('keydown', setEscapeListener);
 }
+
 
 //Метод закрытия попапа через удаление класса
 function closePopup(container) {
   container.classList.remove('popup_opened');
+
+  //удаление обработчика на закрытие попапа через Escape
+  document.removeEventListener('keydown', setEscapeListener);
 }
+
 
 //Общий метод удаления сообщений об ошибке при закрытии окна
 function removeError(form) {
@@ -47,6 +64,7 @@ function removeError(form) {
     hideInputError(form, input);
   });
 }
+
 
 //Общий метод очистки формы попапа
 function resetForm(popup) {
@@ -58,14 +76,9 @@ function setEscapeListener(evt) {
   if(evt.key === 'Escape') {
     const currentPopup = document.querySelector('.popup_opened');
     closePopup(currentPopup);
-    if(currentPopup.classList.contains('popup__form')) {
-      resetForm(currentPopup);
-      removeError(currentPopup);
-    }
-
-    document.removeEventListener('keydown', setEscapeListener);
   }
 }
+
 
 //Общий метод закрытия попапов без сабмита
 const popupAll = document.querySelectorAll('.popup');
@@ -74,13 +87,6 @@ popupAll.forEach(popup => {
     const popupClassesList = evt.target.classList;
     if(popupClassesList.contains('popup__close') || popupClassesList.contains('popup')) {
       closePopup(popup);
-      if(popupClassesList.contains('popup__form')) {
-        resetForm(popup);
-        removeError(popup);
-      }
-
-      //удаление слушателя на закрытие по Escape
-      document.removeEventListener('keydown', setEscapeListener);
     }
   });
 
@@ -91,6 +97,7 @@ popupAll.forEach(popup => {
 function insertNewElementPrepend(where, what) {
   where.prepend(what);
 }
+
 
 //Метод генерации карточки с фото на основе темплейта. Принимает объект с 2-мя ключами (name, link)
 function createNewPhotoCard(obj) {
@@ -119,13 +126,11 @@ function createNewPhotoCard(obj) {
     popupFullScreenPic.src = evt.target.src;
     popupFullScreenPic.alt = evt.target.alt;
     popupFullScreenFigcaption.textContent = evt.target.alt;
-
-    //Добавление слушателя на закрытие по Escape
-    document.addEventListener('keydown', setEscapeListener);
   });
 
   return newPhotoCard;
 }
+
 
 //Загрузка стартовых фото на страницу
 initialCards.forEach((item) => {
@@ -138,34 +143,33 @@ initialCards.forEach((item) => {
 */
 //Открытие попапа добавления пользовательских фото
 photoAddButton.addEventListener('click', () => {
-  openPopup(popupPhotoAdd);
-  //Добавление слушателя на закрытие по Escape
-  document.addEventListener('keydown', setEscapeListener);
+  //очистка формы от старых данных и ошибок перед открытием
+  resetForm(popupPhotoAdd);
+  removeError(popupPhotoAdd);
+
+  setTimeout(() => {
+    enableValidation(photoAddForm, photoAddInputList, photoAddSubmitButton);
+    openPopup(popupPhotoAdd);
+  }, 0);
 });
+
 
 //Добавление польовательского фото на страницу
 popupPhotoAdd.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  //защита от многократной отправки формы
-  const photoButtonSubmit = evt.target.querySelector('.popup__submit');
-  photoButtonSubmit.setAttribute('disabled', 'disabled');
-
+  console.log('yes');
   const userOwnPhoto = {};
   userOwnPhoto.name = photoInputPlaceDescription.value;
   userOwnPhoto.link = photoInputLink.value;
 
+  //очистка формы, вызов слушателя деактивации кнопки сабмита
+  resetForm(popupPhotoAdd);
+
   //вставка элемента в блок
   insertNewElementPrepend(photoSection, createNewPhotoCard(userOwnPhoto));
 
-  //очистка формы ввода при сабмите
-  photoAddForm.reset();
-
   //закрытие попапа
   closePopup(popupPhotoAdd);
-
-  //удаление слушателя на закрытие по Escape
-  document.removeEventListener('keydown', setEscapeListener);
 });
 
 
@@ -174,35 +178,32 @@ popupPhotoAdd.addEventListener('submit', (evt) => {
 */
 //Открытие попапа и предзаполнение формы данными со страницы
 editButton.addEventListener('click', () => {
-  popupForEditFormName.value = userName.textContent;
-  popupForEditFormDescription.value = userDescription.textContent;
-  openPopup(popupForEditForm);
+  //очистка формы от старых данных и ошибок перед открытием
+  resetForm(popupForProfileEditForm);
+  removeError(popupForProfileEditForm);
 
-  //Добавление слушателя на закрытие по Escape
-  document.addEventListener('keydown', setEscapeListener);
-
-  //Валидация формы при первом открытии, до того, как сработает проверка по слушателю на input
-  //Для корректного состояния кнопки submit
-  const editInputsList = Array.from(popupForEditForm.querySelectorAll('.popup__input'));
-  const editSubmitButton = popupForEditForm.querySelector('.popup__submit');
-  toggleButtonState(editInputsList, editSubmitButton);
+  //открытие и предзаполнение попапа
+  setTimeout(() => {
+    enableValidation(profileEditForm, profileEditInputList, profileEditSubmitButton);
+    popupForEditFormName.value = userName.textContent;
+    popupForEditFormDescription.value = userDescription.textContent;
+    openPopup(popupForProfileEditForm);
+  }, 0);
 });
 
+
 //Сохранение новых данных из формы ввода на странице и закрытие попапа при сабмите
-popupForEditForm.addEventListener('submit', (evt) => {
+popupForProfileEditForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  //защита от многократной отправки формы
-  const profileButtonSubmit = evt.target.querySelector('.popup__submit');
-  profileButtonSubmit.setAttribute('disabled', 'disabled');
-
-  //удаление слушателя на закрытие по Escape
-  document.removeEventListener('keydown', setEscapeListener);
 
   //применение введенных данных на странице
   userName.textContent = popupForEditFormName.value;
   userDescription.textContent = popupForEditFormDescription.value;
-  closePopup(popupForEditForm);
+
+  //очистка формы, вызов слушателя деактивации кнопки сабмита
+  resetForm(popupForProfileEditForm);
+
+  closePopup(popupForProfileEditForm);
 });
 
 
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,59 +1,55 @@
-function showInputError(formElement, inputElement, errorMessage) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+function showInputError(singleFormElement, inputElement, errorMessage) {
+  const errorElement = singleFormElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('popup__input_type_error');
   errorElement.classList.add('popup__input-error_active');
   errorElement.textContent = errorMessage;
 }
 
-function hideInputError(formElement, inputElement) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+function hideInputError(singleFormElement, inputElement) {
+  const errorElement = singleFormElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('popup__input_type_error');
   errorElement.classList.remove('popup__input-error_active');
   errorElement.textContent = '';
 }
 
-function checkInputValidity(formElement, inputElement) {
+function checkInputValidity(singleFormElement, inputElement) {
   if(!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(singleFormElement, inputElement, inputElement.validationMessage);
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(singleFormElement, inputElement);
   }
 }
 
-function setEventListener(fieldset) {
-  const inputList = Array.from(fieldset.querySelectorAll('.popup__input'));
-  const buttonSubmit = fieldset.querySelector('.popup__submit');
+function setEventListener(singleFormElement, inputList, buttonSubmit) {
   toggleButtonState(inputList, buttonSubmit);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-      checkInputValidity(fieldset, inputElement);
+      checkInputValidity(singleFormElement, inputElement);
       toggleButtonState(inputList, buttonSubmit);
     });
   });
-}
 
-
-function enableValidation() {
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-  formList.forEach((singleFormElement) => {
-    singleFormElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-    });
-
-    const fieldsetList = Array.from(singleFormElement.querySelectorAll('.popup__fieldset'));
-    fieldsetList.forEach((fieldset) => {
-      setEventListener(fieldset);
-    });
+  singleFormElement.addEventListener('reset', () => {
+    setTimeout(() => {
+      toggleButtonState(inputList, buttonSubmit);
+    }, 0);
   });
 }
 
-enableValidation();
+
+function enableValidation(singleFormElement, inputList, buttonSubmit) {
+  singleFormElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
+  setEventListener(singleFormElement, inputList, buttonSubmit);
+}
+
 
 function hasInvalidInput(inputList) {
-return inputList.some((inputElement) => {
-  return !inputElement.validity.valid;
-})
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
 }
 
 function toggleButtonState(inputList, buttonSubmit) {
